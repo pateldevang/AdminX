ab495b1bd13d8ab65c1435cdf01be7205246c262
pateldevang <devangpatel92@gmail.com>
2019-09-22 00:22:01 +0530

    font added

---
+++ AdminX/loginPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  loginPageViewController.swift
+//  AdminX
+//
+//  Created by Devang Patel on 22/09/19.
+//  Copyright © 2019 Devang Patel. All rights reserved.
+//
+
+import UIKit
+
+class loginPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
